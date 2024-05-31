813659f86877444107d3766242d7bbd4a7e0f2d5
KainTheDev <thica.doi@gmail.com>
2024-05-31 13:51:14 +0700

    .

--- web/scripts/commands.js
+++ web/scripts/commands.js
@@ -1,18 +1,40 @@
 (async () => {
     const contentBox = document.getElementById('contentBox')
+    const optionModalButtons = document.getElementsByName('optionModalButton')
     const commands = await (await fetch('/api/commands')).json()
-    const COMMAND_TYPE = ['Chat Input', 'User', 'Message']
+    const optionsBox = document.getElementById('optionsBox')
+    const COMMAND_TYPES = ['Chat Input', 'User', 'Message']
+    const OPTION_TYPES = ["Sub Command", "Sub Command Group", "String", "Integer", "Boolean", "User", "Channel", "Role", "Mentionable", "Number", "Attachment"];
     for (const { name, description, type } of commands) {
         contentBox.innerHTML += `
-<div class="card bg-black bg-opacity-75" style="width: 18rem;">
+        <div class="card bg-black bg-opacity-75" style="width: 300px">
+            <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">${COMMAND_TYPES[Number(type) - 1]}</h6>
+            <p class="card-text">${description}</p>
+            <hr>
+            <button type="button" class="btn btn-dark bg-black bg-opacity-50" data-bs-toggle="modal" data-bs-target="#optionsModal" name="optionModalButton" command-name= "${name}">Options</button>
+          </div>
+        </div>
+                `
+    }
+    for (const optionModalButton of optionModalButtons) {
+        optionModalButton.addEventListener('click', () => {
+            const commandName = optionModalButton.getAttribute('command-name');
+            const { options } = commands.find(({ name }) => name === commandName)
+            if (options) {
+                optionsBox.innerHTML = options.map(({ name, description, type, required }) => `
+                    <div class="card bg-black bg-opacity-75" style="width: 300px">
     <div class="card-body">
     <h5 class="card-title">${name}</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">${COMMAND_TYPE[type]}</h6>
+    <h6 class="card-subtitle mb-2 text-body-secondary">${OPTION_TYPES[Number(type) - 1]}</h6>
     <p class="card-text">${description}</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
+    <hr>
+    ${type === 1 ? '' : `<p><b>${required ? "Required." : "Not required."}</b></p>`}
   </div>
 </div>
-        `
+                    `).join('')
+            } else return optionsBox.innerHTML = "None."
+        })
     }
 })()
