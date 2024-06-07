adce1845c6e5fff36da9b7f2b9e9e16476840f2e
KainTheDev <thica.doi@gmail.com>
2024-06-07 16:16:20 +0700

    .

--- web/scripts/commands.js
+++ web/scripts/commands.js
@@ -5,39 +5,13 @@
     const commandsBox = document.getElementById('commandsBox');
     const JSONButton = document.getElementsByName('JSONButton');
     const JSONDisplayBLock = document.getElementById('JSONDisplayBLock')
+    const optionTypes = document.getElementById('optionTypes')
+    const commandTypes = document.getElementById('commandTypes')
     const JSONModal = new bootstrap.Modal(document.getElementById('JSONModal'), {
         backdrop: 'static',
         keyboard: false
     })
     const commands = await (await fetch('api/commands')).json();
-    for (const command of commands) {
-        if (!command.options) continue;
-        command.subCommandGroups = command.options.filter(({ type }) => type === 2)
-        command.subCommands = command.options.filter(({ type }) => type === 1)
-        command.subCommandOptions = []
-        if (command.subCommandGroups.length > 0) {
-            for (const subCommandGroup of command.subCommandGroups) {
-                for (const subCommand of subCommandGroup.options) {
-                    subCommand.parent = subCommandGroup.name
-                    command.subCommands.push(subCommand)
-                    if (subCommand.options) {
-                        for (const option of subCommand.options) {
-                            option.parent = subCommand.name
-                            command.subCommandOptions.push(option)
-                        }
-                    }
-                }
-            }
-        } else if (command.subCommands.length > 0) {
-            for (const subCommand of command.subCommands) {
-                if (!subCommand.options) continue;
-                for (const option of subCommand.options) {
-                    option.parent = subCommand.name
-                    command.subCommandOptions.push(option)
-                }
-            }
-        }
-    }
     const COMMAND_TYPES = ['Chat Input', 'User', 'Message'];
     const OPTION_TYPES = ["Sub Command", "Sub Command Group", "String", "Integer", "Boolean", "User", "Channel", "Role", "Mentionable", "Number", "Attachment"];
 
@@ -65,35 +39,36 @@
 
     // Adding command rows to the table
     commands.forEach((command, index) => {
-        const { name, description, type, subCommands, subCommandGroups } = command
+        const { name, description, type } = command
+        const optionTypes = []
+        const commandTypes = []
+        for (const optionType in OPTION_TYPES) {
+            optionTypes.push(`${Number(optionType) + 1} = ${OPTION_TYPES[optionType]}`)
+        }
+        for (const commandType in COMMAND_TYPES) {
+            commandTypes.push(`${Number(commandType) + 1} = ${COMMAND_TYPES[commandType]}`)
+        }
         const commandCard = document.createElement('div')
         commandCard.className = 'bg-dark bg-opacity-25 rounded p-4 d-flex flex-column'
-        const additonalString = []
-        if (subCommandGroups) {
-            for (const subCommandGroup of subCommandGroups) {
-                for (const subCommand of subCommands) {
-                    if (subCommand.parent === subCommandGroup.name) {
-                        additonalString.push(`${subCommandGroup.name} ${subCommand.name}`)
-                    }
-                }
-            }
-        }
-        if (subCommands) {
-            for (const { name } of subCommands) {
-                additonalString.push(name)
-            }
-        }
-        commandCard.innerHTML = `<h2><span class="text-secondary-emphasis" style="font-size: 24px">${index + 1}. </span>${name}</h2><h5 class="text-secondary">${COMMAND_TYPES[Number(type) - 1]}</h5><p>${description}</p>${additonalString.map(string => `<div class='bg-black bg-opacity-75 p-2 mt-3 rounded'>/${name} ${string}</div>`).join('')}<hr><button class="btn btn-dark border border-0" json="${encodeURI(JSON.stringify(command, null, '·'))}" name="JSONButton">View JSON data</button>`
+        commandCard.innerHTML = `<h2><span class="text-secondary-emphasis" style="font-size: 24px">${index + 1}. </span>${name}</h2><h5 class="text-secondary">${COMMAND_TYPES[Number(type) - 1]}</h5><p>${description}</p><hr class="mt-auto"><button class="btn btn-dark border border-0" json="${encodeURI(JSON.stringify(command, null, '·'))}" name="JSONButton" commandTypes="${commandTypes.join('\n')}" optionTypes="${optionTypes.join('\n')}">View JSON data</button>`
         commandCard.style.width = '250px'
         commandsBox.appendChild(commandCard)
     })
     for (const button of JSONButton) {
         button.addEventListener('click', () => {
             const json = button.getAttribute('json')
+            const optionTypesString = button.getAttribute('optionTypes')
+            const commandTypesString = button.getAttribute('commandTypes')
+            optionTypes.innerHTML = optionTypesString
+            commandTypes.innerHTML = commandTypesString
             JSONDisplayBLock.innerHTML = decodeURI(json).split('·').join(' ')
             JSONModal.show()
             hljs.highlightElement(JSONDisplayBLock);
+            hljs.highlightElement(commandTypes);
+            hljs.highlightElement(optionTypes);
             JSONDisplayBLock.removeAttribute('data-highlighted')
+            commandTypes.removeAttribute('data-highlighted')
+            optionTypes.removeAttribute('data-highlighted')
         })
     }
 })();
