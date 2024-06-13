67e613b88964ca2d4d335375957fc5fbf3422fcb
KainTheDev <thica.doi@gmail.com>
2024-06-13 16:28:24 +0700

    .

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ require('dotenv').config()
 // Import the express module
 const express = require('express');
 const { readdirSync } = require('fs');
-
+const { join } = require('path');
 // Create an Express application
 const app = express();
 
-app.use(express.static(`${process.cwd()}/web`))
+app.use(express.static(join(process.cwd(), 'web')))
 
-const pages = readdirSync('web/pages')
+const pages = readdirSync(join(process.cwd(), 'web', 'pages'))
 for (const page of pages) {
     const pageName = page.split('.')[0]
-    app.get(`/${pageName}`, (req, res) => res.sendFile(`${process.cwd()}/web/pages/${page}`))
+    app.get(`/${pageName}`, (req, res) => res.sendFile(join(process.cwd(), 'web', 'pages', `${page}`)))
 }
 
 const apis = readdirSync('api')
 for (const api of apis) {
     const apiRoute = api.split('.')[0]
-    app.get(`/api/${apiRoute}`, require(`${process.cwd()}/api/${apiRoute}`))
+    app.get(`/api/${apiRoute}`, require(join(process.cwd(), 'api', `${apiRoute}`)))
 }
 
-    const redirects = require('./json/redirects.json')
+const redirects = require(join(process.cwd(), 'json', 'redirects.json'));
 for (const { name, url } of redirects) {
     app.get(name, (req, res) => res.redirect(url))
 }
