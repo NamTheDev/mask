7753a09601858bf1c63d2fe3113ebc5f12025be5
KainTheDev <thica.doi@gmail.com>
2024-06-22 13:35:13 +0700

    .

--- web/scripts/card.js
+++ web/scripts/card.js
@@ -1,31 +1,38 @@
-const footer = document.getElementById('footer')
-const title = document.getElementById('title')
-const subtitle = document.getElementById('subtitle')
-const nicknamesBox = document.getElementById('nicknamesBox')
-const card = document.getElementById('card')
+(async () => {
+    const footer = document.getElementById('footer')
+    const title = document.getElementById('title')
+    const subtitle = document.getElementById('subtitle')
+    const nicknamesBox = document.getElementById('nicknamesBox')
+    const card = document.getElementById('card')
+    const { ownerNicknames } = await (await fetch('/config.json')).json()
+    const description = document.getElementById('description')
+    const descriptionContent = await (await fetch('/assets/description.md')).text()
+    const converter = new showdown.Converter()
 
-function handleScreenWidthChange(event) {
-    if (event.matches) {
-        // Screen width is less than 650px
-        footer.classList.replace('mx-5', 'mx-2');
-        nicknamesBox.classList.add('flex-column')
-    } else {
-        // Screen width is 650px or greater
-        footer.classList.replace('mx-2', 'mx-5');
-        nicknamesBox.classList.remove('flex-column')
+    description.innerHTML = converter.makeHtml(descriptionContent);
+
+    function handleScreenWidthChange(event) {
+        if (event.matches) {
+            // Screen width is less than 650px
+            footer.classList.replace('mx-5', 'mx-2');
+            nicknamesBox.classList.add('flex-column')
+        } else {
+            // Screen width is 650px or greater
+            footer.classList.replace('mx-2', 'mx-5');
+            nicknamesBox.classList.remove('flex-column')
+        }
     }
-}
 
-const mediaQuery = window.matchMedia('(max-width: 650px)');
-mediaQuery.addEventListener("change", handleScreenWidthChange);
+    const mediaQuery = window.matchMedia('(max-width: 650px)');
+    mediaQuery.addEventListener("change", handleScreenWidthChange);
 
-// Initial check
-handleScreenWidthChange(mediaQuery);
+    // Initial check
+    handleScreenWidthChange(mediaQuery);
 
-const year = new Date().getFullYear() - 2009
-subtitle.textContent = subtitle.textContent.replace('{age}', year)
+    const year = new Date().getFullYear() - 2009
+    subtitle.textContent = subtitle.textContent.replace('{age}', year)
 
-const nicknames = ['Beam', 'Namir', 'Sharkboy', 'Nambuger', 'Disgusting', 'Idiot']
-for (const nickame of nicknames) {
-    nicknamesBox.innerHTML += `<div class='d-flex flex-wrap'><span class='bg-dark bg-opacity-50 p-2 rounded fw-semibold' style='font-size: 20px'>${nickame}</span></div>`
-}
+    for (const nickame of ownerNicknames) {
+        nicknamesBox.innerHTML += `<div class='d-flex flex-wrap'><span class='bg-black bg-opacity-50 p-2 rounded fw-semibold' style='font-size: 14px'>${nickame}</span></div>`
+    }
+})()
