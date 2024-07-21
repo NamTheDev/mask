ee06ac00237ab6bd7c45d0b94a3d64dcb180070c
KainTheDev <thica.doi@gmail.com>
2024-07-21 08:08:24 +0700

    .

--- api/getConsoleData.js
+++ api/getConsoleData.js
@@ -8,7 +8,7 @@ const { consoleData } = require("../config")
 module.exports = async (req, res) => {
     const {key} = req.query
     if(key !== process.env.SECRET_KEY) return;
-    const {data} = consoleData
+    const data = Object.entries(consoleData.data).map(([key, value]) => ({key, value}))
     if (data.length > 0) {
         res.json({ data: data.map(({ key, value }) => `<b>${key}</b>: ${value}<br>`).join('\n') })
     } else {
